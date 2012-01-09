c09bb7556672424505ee18300c6983ee19421727
Filip Wiśniewski <ffwisniewski@gmail.com>
2012-01-09 14:12:05 +0100

    scaffold for SQL connector

--- src/main/java/es/upm/fi/dia/oeg/map4rdf/server/bootstrap/Bootstrapper.java
+++ src/main/java/es/upm/fi/dia/oeg/map4rdf/server/bootstrap/Bootstrapper.java
@@ -33,8 +33,10 @@ public class Bootstrapper extends GuiceServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
               	
-                SQLconnector dbConnector = Guice.createInjector().getInstance(SQLconnector.class);
-                
+             
+            
+               //SQLconnector dbConnector = Guice.createInjector().getInstance(SQLconnector.class);
+             
                 InputStream propIn = servletContextEvent.getServletContext().getResourceAsStream(Constants.CONFIGURATION_FILE);
 		try {
 			config = new Configuration(propIn);
